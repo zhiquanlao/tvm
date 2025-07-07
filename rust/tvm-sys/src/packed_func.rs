f13519160216369a4afced636964f88a8dd42f1d
Zhiquan Lao <114715915+zhiquanlao@users.noreply.github.com>
2025-07-07 03:17:29 -0400

    Update packed_func.rs

--- rust/tvm-sys/src/packed_func.rs
+++ rust/tvm-sys/src/packed_func.rs
@@ -16,7 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
+#[allow(non_upper_case_globals)]
+pub const TVMArgTypeCode_kTVMArgBool: ffi::TVMArgTypeCode =
+    ffi::kTVMArgBool;
 use std::{
     convert::TryFrom,
     ffi::{CStr, CString},
